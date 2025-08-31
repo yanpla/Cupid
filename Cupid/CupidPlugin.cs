66de45f19fdb1d9f8e30a17611971e245ebb42fd
yanpla <yannick@plantenga.com>
2025-09-01 00:19:28 +0200

    Initial commit

---
+++ Cupid/CupidPlugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using MiraAPI;
+using MiraAPI.PluginLoading;
+using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+using Reactor.Utilities;
+
+namespace Cupid;
+
+[BepInAutoPlugin("yanpla.Cupid", "Cupid")]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+[BepInDependency(MiraApiPlugin.Id)]
+[ReactorModFlags(ModFlags.RequireOnAllClients)]
+public partial class CupidPlugin : BasePlugin, IMiraPlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public string OptionsTitleText => "Cupid";
+
+    public ConfigFile GetConfigFile() => Config;
+
+    public override void Load()
+    {
+        ReactorCredits.Register(Name, Version, false, ReactorCredits.AlwaysShow);
+        Harmony.PatchAll();
+    }
+}
