8ed9a98634bf47a510936e56237f901731e89f07
yanpla <yannick@plantenga.com>
2025-09-02 00:31:07 +0200

    Enhance Cupid role with advanced description and ability details

--- Cupid/Roles/CupidRole.cs
+++ Cupid/Roles/CupidRole.cs
@@ -1,30 +1,40 @@
+using Il2CppInterop.Runtime.Attributes;
 using MiraAPI.Modifiers;
 using MiraAPI.Roles;
 using MiraAPI.Utilities.Assets;
 using Reactor.Networking.Attributes;
 using Reactor.Utilities;
 using TownOfUs;
+using TownOfUs.Assets;
+using TownOfUs.Extensions;
 using TownOfUs.Modifiers.Game.Alliance;
+using TownOfUs.Modules.Wiki;
+using TownOfUs.Roles;
 using TownOfUs.Utilities;
 using UnityEngine;
 
 namespace Cupid.Roles;
 
-public class CupidRole : CrewmateRole, ICustomRole
+public class CupidRole : CrewmateRole, ITownOfUsRole, IWikiDiscoverable, IDoomable
 {
     public static List<PlayerControl> SelectedPlayers { get; } = new();
     
     public string RoleName => "Cupid";
-    public string RoleLongDescription => "Make two players fall in love. If one lover dies, the other dies too. (1st round only, players become Lovers after first meeting)";
-    public string RoleDescription => "Make two players fall in love.";
+    public string RoleLongDescription => "Spread love between two players.\n" +
+                                         "If one lover dies, the other dies too.\n";
+    public string RoleDescription => "Spread love between two players.";
+    
+    public RoleAlignment RoleAlignment => RoleAlignment.CrewmatePower;
+    
     public Color RoleColor { get; } = Color.magenta;
     public ModdedRoleTeams Team => ModdedRoleTeams.Crewmate;
     public RoleOptionsGroup RoleOptionsGroup { get; } = RoleOptionsGroup.Crewmate;
+    public DoomableType DoomHintType { get; } = DoomableType.Trickster;
     public CustomRoleConfiguration Configuration => new()
     {
         MaxRoleCount = 1,
         OptionsScreenshot = MiraAssets.Empty,
-        Icon = MiraAssets.Empty,
+        Icon = Assets.CupidShootButton,
         CanGetKilled = true,
         UseVanillaKillButton = false,
         CanUseVent = false,
@@ -36,6 +46,22 @@ public class CupidRole : CrewmateRole, ICustomRole
         RoleHintType = RoleHintType.RoleTab,
         ShowInFreeplay = true,
     };
+    
+    public string GetAdvancedDescription()
+    {
+        return
+            $"The {RoleName} is a Crewmate Power role that can shoot arrows to bind two players together as Lovers. If one Lover dies, the other will follow. (Make sure to shoot before the first meeting!)" +
+            MiscUtils.AppendOptionsText(GetType());
+    }
+    
+    [HideFromIl2Cpp]
+    public List<CustomButtonWikiDescription> Abilities { get; } =
+    [
+        new("Shoot love arrows",
+            "Fire a magical arrow at a player to mark them as a potential Lover." + 
+            " When two players are marked, they will be bound together as Lovers at the end of the first meeting.",
+            Assets.CupidShootButton)
+    ];
 
     public static void onRoundStart()
     {
